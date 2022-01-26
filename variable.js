90b9eeb0836578a9b9492f67005e2c5332f04261
Kamrul Hasan <you@example.com>
2022-01-26 22:26:18 +0600

    variabel recap

---
+++ variable.js
@@ -0,0 +1,13 @@
+//numeric type variable
+var watchPrice = 120;
+watchPrice = 125;
+
+//string type variable
+var userName = 'Kamrul Hasan';
+
+//boolean type variable
+var isExpensive = true;
+
+// interger 1, 2, 3, 4, 5
+// float 2.2, 1.5, 5.4343
+
