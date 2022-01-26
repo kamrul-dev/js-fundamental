805fce57f2afc1ea7b92771ba7c59f56f867710a
Kamrul Hasan <you@example.com>
2022-01-26 22:43:02 +0600

    Declare array and find the array length

---
+++ array.js
@@ -0,0 +1,15 @@
+var firendsAge = [11, 13, 17];
+
+var collectionFee = [34, 55, 221, 3];
+
+var fruits = ['apple', 'orange', 'bannana', "pineapple"];
+
+var oddNumbers = [1, 3, 5, 7, 9];
+
+var vowels = ['a', 'e', 'i', '0', 'u'];
+
+// console.log(vowels);
+
+var vowelsCount = vowels.length;
+console.log(vowelsCount);
+// console.log(vowels.length);
