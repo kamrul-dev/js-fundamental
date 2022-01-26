96874f6a2b00a31eb2a86e546c32678f8c6e0bb0
Kamrul Hasan <you@example.com>
2022-01-27 01:50:26 +0600

    Handle multiple conditions, and or

---
+++ multiple-condition.js
@@ -0,0 +1,50 @@
+// var gotJob = true;
+
+// if (gotJob == true){
+//     console.log('Cholo bia kore feli');
+// }
+// else{
+//     console.log('tor kopale bia nai');
+
+// }
+
+// var gotJob = false;
+// var moneySaved = 150000;
+
+// if (gotJob == true && moneySaved > 200000){
+//     console.log('Cholo bia kore feli');
+// }
+// else{
+//     console.log('tor kopale bia nai');
+// }
+
+
+// var gotJob = true;
+// var moneySaved = 250000;
+// var hasFalt = false;
+
+// if (gotJob == true && moneySaved > 200000 && hasFalt == true){
+//     console.log('Cholo bia kore feli');
+// }
+// else{
+//     console.log('tor kopale bia nai');
+// }
+
+var gotJob = true;
+var moneySaved = 50000;
+var hasFalt = false;
+var hasHouse = true;
+
+// if (gotJob == true || moneySaved > 200000){
+//     console.log('Cholo bia kore feli');
+// }
+// else{
+//     console.log('tor kopale bia nai');
+// }
+
+if ((gotJob == true && moneySaved > 200000) || hasHouse == true){
+    console.log('Cholo bia kore feli');
+}
+else{
+    console.log('tor kopale bia nai');
+}
