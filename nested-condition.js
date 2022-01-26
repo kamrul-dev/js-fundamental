de26edd67bd4108d5ba4919584668e8533cce4b0
Kamrul Hasan <you@example.com>
2022-01-27 02:03:01 +0600

    (advanced) Multi stage condition and nested conditions

---
+++ nested-condition.js
@@ -0,0 +1,28 @@
+var danishPrice = 50;
+var butterBreadPrice = 25;
+var toastBiscuitPrice = 10;
+var myBudget = 7;
+var packedWell = false;
+
+// if(danishPrice < myBudget){
+//     console.log('Danish kheye vanish hoia jabo');
+// }
+// else if(butterBreadPrice < myBudget){
+//     console.log('butter bon dia cha khabo');
+    
+// }
+// else if(toastBiscuitPrice < myBudget){
+//     console.log('toast dia cha khabo')
+// }
+// else{
+//     console.log('batasha dia khabo');
+// }
+
+if(danishPrice < myBudget){
+    if(packedWell == true){
+        console.log('I will eat');
+    }
+    else{
+        console.log('I will not eat')
+    }
+}
