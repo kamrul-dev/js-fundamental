22f4c32a61e0665e9fb182089c9325726e7a5d9f
Kamrul Hasan <you@example.com>
2022-01-26 23:41:58 +0600

    Array index, get and set by index, indexOf

---
+++ array-index.js
@@ -0,0 +1,16 @@
+var books = ['hablu', 'bolod', 'battery', 'peracitamol', 'programm', 'story'];
+
+var batteryIndex = books.indexOf('battery');
+// console.log(batteryIndex);
+
+
+var numbers = [23, 34, 35, 53, 25, 53, 534,];
+var index = numbers.indexOf(53);
+// console.log(index);
+
+var secondIndex = books[2];
+// console.log(secondIndex);
+console.log(numbers);
+
+numbers[1] = 33;
+console.log(numbers);
