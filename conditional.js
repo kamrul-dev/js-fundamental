fad4008451b2a89c0ac1a3303b59b1d18104f504
Kamrul Hasan <you@example.com>
2022-01-27 01:19:09 +0600

    Make conditional decision, if-else, comparison

---
+++ conditional.js
@@ -0,0 +1,41 @@
+var isFoodReady = true;
+// Jodi food ready hoi tahole ami khabo
+// if food is ready then i will eat
+// if food is ready == true
+
+// if (condition) {    
+
+// }
+
+// if(isFoodReady == true){
+//     console.log('Mama !! I Will eat now.');
+// }
+
+var iphonePrice = 70000;
+var myBugget = 109500;
+
+// if (iphonePrice < myBugget){
+//     console.log('I will buy Iphone');
+// }
+
+// if(iphonePrice > myBugget){
+//     console.log('My samsung is the best');
+// }
+
+// if (condition){
+
+// }
+// else{
+
+// }
+
+
+var chickenPirce = 180;
+var myMoney = 50;
+
+if (chickenPirce < myMoney){
+    console.log('Yes ! I Will eat chicken leg')
+}
+else{
+    console.log('Smashed potato with lentil soup')
+}
