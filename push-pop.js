28e0f7de842c1db203fea308c795f12942b74aaf
Kamrul Hasan <you@example.com>
2022-01-27 00:26:32 +0600

    Add or remove element from array using push, pop

---
+++ push-pop.js
@@ -0,0 +1,20 @@
+// Add elements to an array
+
+var lastBench = ['kamrul', 'hasan', 'babu'];
+lastBench.push('kamal');
+lastBench.push('jamal');
+lastBench.push('kalam');
+
+// console.log(lastBench);
+
+var friendsAge = [11, 12, 14, 15, 16];
+// friendsAge.push(20);
+ console.log(friendsAge);
+
+//remove elements from an array
+var lastItem = friendsAge.pop();
+
+// friendsAge.pop();
+// friendsAge.pop();
+console.log(friendsAge);
+console.log(lastItem);
